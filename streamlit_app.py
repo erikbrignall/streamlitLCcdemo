c949eaab00c76facb01f1d9429c95b3de3d8cea4
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2023-08-30 14:25:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,17 @@ Created on Fri Sep  9 09:08:31 2022
 
 import streamlit as st
 import pandas as pd
-import pandas as pd 
-#import matplotlib.pyplot as plt 
-#import seaborn as sns 
-from langchain.agents import create_pandas_dataframe_agent 
-from langchain.llms import OpenAI 
 from langchain.chat_models import ChatOpenAI
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.agents.agent_types import AgentType
+
+st.image('logo-temp2.PNG', width=200)
+
+openai_api_key = st.secrets["OpenAIapikey"]
+
+# Page title
+st.set_page_config(page_title='GA4 Analyser')
+st.title('GA4 Analysis bot')
 
 filename = 'origin_customers.csv'
 df = pd.read_csv(filename) 
@@ -20,21 +25,31 @@ filename = 'products_sold.csv'
 df1 = pd.read_csv(filename) 
 dfs = [df,df1]
 
-st.image('logo-temp2.PNG', width=200)
-st.title('🦜🔗 GA4 Analysis Bot')
+# Generate LLM response
+def generate_response(csv_file, input_query):
+  llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.2, openai_api_key=openai_api_key)
+  #df = load_csv(csv_file)
+  # Create Pandas DataFrame Agent
+  agent = create_pandas_dataframe_agent(llm, dfs, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
+  # Perform Query using the Agent
+  response = agent.run(input_query)
+  return st.success(response)
 
-openai_api_key = st.secrets["OpenAIapikey"]
+# Input widgets
+uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
+question_list = [
+  'How many rows are there?',
+  'What is the range of values for MolWt with logS greater than 0?',
+  'How many rows have MolLogP value greater than 0.',
+  'Other']
+query_text = st.selectbox('Select an example query:', question_list, disabled=not uploaded_file)
+openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
 
-def generate_response(input_text):
-  #llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-  agent = create_pandas_dataframe_agent(OpenAI(temperature=0, model_name='gpt-3.5-turbo',openai_api_key=openai_api_key), dfs, verbose=True)
-  #st.info(llm(input_text))
-  st.info(agent(input_text))
-
-with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
+# App logic
+if query_text is 'Other':
+  query_text = st.text_input('Enter your query:', placeholder = 'Enter query here ...', disabled=not uploaded_file)
+if not openai_api_key.startswith('sk-'):
+  st.warning('Please enter your OpenAI API key!', icon='⚠')
+if openai_api_key.startswith('sk-') and (uploaded_file is not None):
+  st.header('Output')
+  generate_response(uploaded_file, query_text)
